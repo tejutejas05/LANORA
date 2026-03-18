94b7c431f4561f05b1fc570fd33b18b38ae998fd
Prashanth J N <prashi8711@gmail.com>
2026-03-18 17:05:32 +0530

    react practice

--- frontend/react basics/react-demo-app/src/App.tsx
+++ frontend/react basics/react-demo-app/src/App.tsx
@@ -1,10 +1,19 @@
-import ListGroup from "./components/ListGroup.tsx";
-import Events from "./components/Events.tsx";
-function App() {
-   
-  return <div><Events/> <ListGroup/></div>
+import ListGroup from "./components/ListGroup";
+import Events from "./components/Events";
+import Alert from "./components/Alert.js";
+import Button from "./components/Button.js"
 
-  
+let names=['alice','bob','car']
+function App() {
+  return <div>
+    <Button color="primary" onClick={()=> console.log("clicked")}>
+      the green button
+    </Button>
+    <ListGroup/>
+    <Alert> an alert messsage</Alert>
+    <ListGroup/>
+    <Events name={names} heading="names" onSelectItem={(names) => console.log(names)}></Events>
+   </div>
 }
 
 export default App
