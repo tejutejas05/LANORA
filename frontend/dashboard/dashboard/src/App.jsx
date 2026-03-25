231f06e057d96a613454c0d94cda659f2a3f6268
Prashanth J N <prashi8711@gmail.com>
2026-03-25 16:46:27 +0530

    front-page initalisation

---
+++ frontend/dashboard/dashboard/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className="dashboard">
+      <h1>Welcome to lanora !!</h1>
+
+     </div>
+    </>
+  )
+}
+
+export default App
