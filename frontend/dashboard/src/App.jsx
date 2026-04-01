6ac01f3571570d5f85bfb0072f765645ed749643
Prashanth J N <prashi8711@gmail.com>
2026-04-01 18:43:16 +0530

    abx

---
+++ frontend/dashboard/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import './home/Navbar'
+import {Navbar} from './home/Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>dashboard</h1>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
