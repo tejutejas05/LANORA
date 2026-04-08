f29bd69025150a3d7c2c401fa4a9fe37a582298e
tejutejas05 <tejasmr05@gmail.com>
2026-04-08 19:12:47 +0530

    postgres connection done

--- backend-GO/main.go
+++ backend-GO/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"net/http"
 	"fmt"
+
+	"lanora-backend/database"
+	
 )
 
 func main(){
-	fmt.Println("Hello World")
+	database.Connect()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Server is running")
+	})
+
+	fmt.Println("Server started at :8080")
+	http.ListenAndServe(":8080", nil)
+
 }
+
