3233342cf671912f158fad5dd6cb9e0a4117766d
tejutejas05 <tejasmr05@gmail.com>
2026-04-19 12:26:04 +0530

    Clean code for test.go

--- backend-GO/handlers/test.go
+++ backend-GO/handlers/test.go
@@ -6,96 +6,102 @@ import(
 	"net/http"
 	"os"
 	"encoding/json"
-
-	"lanora-backend/models"
+	"path/filepath"
+	//"lanora-backend/models"
 	"lanora-backend/services"
 
 )
 
 func TestAgent(w http.ResponseWriter, r *http.Request) {
 
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	fmt.Println("/test-agent hit")
+
+	//parse uploaded zip
+
+	err := r.ParseMultipartForm(10 << 20)
+	if err != nil {
+		http.Error(w, "Cannot parse Form", http.StatusBadRequest)
+		return 
+	}
 
 	file, handler, err := r.FormFile("file")
 	if err != nil {
-		http.Error(w, "File not recevied", 400)
+		http.Error(w, "file not received", http.StatusBadRequest)
 		return
 	}
 	defer file.Close()
 
-	fmt.Println("file recevied:", handler.Filename)
+	// save uploaded zip 
 
-	// save file locally for temp
-	out, err := os.Create(handler.Filename)
-	if err != nil{
-		http.Error(w, "Cannot save file", 500)
-		return 
+	uploadDir := "uploads"
+	os.MkdirAll(uploadDir, os.ModePerm)
+
+	filePath := filepath.Join(
+		uploadDir,
+		handler.Filename,
+	)
+
+	out , err := os.Create(filePath)
+	if err != nil {
+		http.Error(w, "cannot save the file", http.StatusInternalServerError)
+		return
 	}
 	defer out.Close()
 
-	io.Copy(out, file)
-
-	fmt.Println("File saved successfully!!")
-}
-
-// func TestAgentHandler(w http.ResponseWriter, r *http.Request) {
-	
-// 	var req models.TestAgentRequest
-
-// 	err := json.NewDecoder(r.Body).Decode(&req)
-// 	if err != nil {
-// 		http.Error(w, "Invalid request", http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	logs, err := map[string]interface{}{}
-
-// 	if err != nil {
-// 		response["status"] = "error"
-// 		response["error"] = err.Error()
-// 	}
-// 	else{
-// 		response["status"] = "success"
-// 		response["logs"] = logs
-// 	}
-
-// 	w.Header().Set("Content-Type", "application/json")
-// 	json.NewEncoder(w).Encode(response)
-// }
-
-//docker part
-
-func RunAgent(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method != "POST" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return 
-	}
-
-	var req models.TestAgentRequest
-
-	err := json.NewDecoder(r.Body).Decode(&req)
-
+	_, err = io.Copy(out, file)
 	if err != nil {
-		http.Error(w, "invalid Request", http.StatusBadRequest)
+		http.Error(w, "failed to save the file", http.StatusInternalServerError)
 		return
 	}
 
-	logs, err := services.RunDockerContainer(req.Image)
+	fmt.Println("Zip saved at : ", filePath)
+
+	// --------------------------jev---------------------------------
+
+	imagename ,err := services.BuildDockerImage(filePath)
+
+	if err != nil {
+		
+		response := map[string] interface{} {
+			"status": "error",
+			"stage": "build",
+			"error": err.Error(),
+		}
+
+		json.NewEncoder(w).Encode(response)
+		return 
+	}
+
+	//------------------------------------------------------------------------
+
+
+	// here i will take the logs from the running container
+
+	logs, err := services.RunDockerContainer(imagename)
 
 	response := map[string]interface{}{}
 
 	if err != nil {
+
 		response["status"] = "error"
+		response["stage"] = "runtime"
 		response["error"] = err.Error()
+		response["logs"] = logs
 	} else {
 		response["status"] = "success"
 		response["logs"] = logs
 	}
 
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set(
+		"Content-Type",
+		"application/json",
+	)
+
 	json.NewEncoder(w).Encode(response)
 
-
 }
-
